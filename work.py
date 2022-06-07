75ff4a6d572b79d5bc2053c9a0e95281c5ff76fd
Daniel Merser <91954962+M2101479@users.noreply.github.com>
2022-06-07 16:03:00 +0000

    Initial commit

---
+++ work.py
@@ -0,0 +1,83 @@
+# Import Module
+from tkinter import *
+ 
+# create root window
+root = Tk()
+ 
+# root window title and dimension
+root.title("Name finder")
+# Set geometry(widthxheight)
+root.geometry('350x200')
+ 
+# adding menu bar in root window
+# new item in menu bar labelled as 'New'
+# adding more items in the menu bar
+menu = Menu(root)
+item = Menu(menu)
+item.add_command(label='New')
+menu.add_cascade(label='File', menu=item)
+root.config(menu=menu)
+ 
+# adding a label to the root window
+lbl = Label(root, text = "What is your name?")
+lbl.grid()
+ 
+# adding Entry Field
+txt = Entry(root, width=10)
+txt.grid(column =1, row =0)
+ 
+ 
+# function to display user text when
+# button is clicked
+def clicked():
+ 
+    res = "You wrote " + txt.get()
+    lbl.configure(text = res)
+ 
+# button widget with red color text inside
+btn = Button(root, text = "Click me" ,
+             fg = "red", command=clicked)
+# Set Button Grid
+btn.grid(column=2, row=0)
+
+class MyWindow:
+    def __init__(self, win):
+        self.lbl1=Label(win, text='First number')
+        self.lbl2=Label(win, text='Second number')
+        self.lbl3=Label(win, text='Result')
+        self.t1=Entry(bd=3)
+        self.t2=Entry()
+        self.t3=Entry()
+        self.btn1 = Button(win, text='Add')
+        self.btn2=Button(win, text='Subtract')
+        self.lbl1.place(x=100, y=50)
+        self.t1.place(x=200, y=50)
+        self.lbl2.place(x=100, y=100)
+        self.t2.place(x=200, y=100)
+        self.b1=Button(win, text='Add', command=self.add)
+        self.b2=Button(win, text='Subtract')
+        self.b2.bind('<Button-1>', self.sub)
+        self.b1.place(x=100, y=150)
+        self.b2.place(x=200, y=150)
+        self.lbl3.place(x=100, y=200)
+        self.t3.place(x=200, y=200)
+    def add(self):
+        self.t3.delete(0, 'end')
+        num1=int(self.t1.get())
+        num2=int(self.t2.get())
+        result=num1+num2
+        self.t3.insert(END, str(result))
+    def sub(self, event):
+        self.t3.delete(0, 'end')
+        num1=int(self.t1.get())
+        num2=int(self.t2.get())
+        result=num1-num2
+        self.t3.insert(END, str(result))
+window=Tk()
+mywin=MyWindow(window)
+window.title('Hello Python')
+window.geometry("400x300+10+10")
+window.mainloop()
+ 
+# Execute Tkinter
+root.mainloop()
